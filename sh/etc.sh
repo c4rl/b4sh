fe3f8031d29ad9b0d68255dc07d71701883a33db
Carl Wiedemann <carl.wiedemann@gmail.com>
2012-12-04 14:38:54 -0500

    Compress and remove

--- sh/etc.sh
+++ sh/etc.sh
@@ -15,3 +15,11 @@ function ptp() {
 function pte() {
   echo "http://pt.c4rl.ws/e/$1" | tr -d "\n" | pbcopy && echo "http://pt.c4rl.ws/e/$1"
 }
+
+# Other stuff
+
+# Shortcut to compress and remove a file or directory.
+function czf() {
+  FILE=$1
+  tar -czf $FILE.tar.gz $FILE && rm -rf $FILE
+}
