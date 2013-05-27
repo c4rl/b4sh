aca88b2728adb91ae37a4b119e662e397dfac6c1
Carl Wiedemann <carl.wiedemann@gmail.com>
2013-05-27 14:01:20 -0600

    Daisy diff integration

---
+++ sh/diff.sh
@@ -0,0 +1,13 @@
+function ddiff() {
+  URL=$1
+  # Get clean output
+  http d8clean.localhost/$URL Cookie:$DRUPALCOOKIECLEAN > /tmp/d8clean-tmp.html
+  # Get patched output
+  http d8.localhost/$URL Cookie:$DRUPALCOOKIE > /tmp/d8.html
+  # Make domains similar
+  sed -e 's/d8clean/d8/g' /tmp/d8clean-tmp.html > /tmp/d8clean.html
+  # Kaleidescope diff
+  ksdiff /tmp/d8clean.html /tmp/d8.html
+}
+
+alias daisyd='java -jar daisydiff.jar /tmp/d8clean.html /tmp/d8.html && open daisydiff.htm'
