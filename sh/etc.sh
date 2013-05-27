dbd227d25b2b757233544a79a4a486c5449e4565
Carl Wiedemann <carl.wiedemann@gmail.com>
2013-05-27 14:02:09 -0600

    Some filesystem stuff

--- sh/etc.sh
+++ sh/etc.sh
@@ -16,8 +16,17 @@ function pte() {
   echo "http://pt.c4rl.ws/e/$1" | tr -d "\n" | pbcopy && echo "http://pt.c4rl.ws/e/$1"
 }
 
+# UNIX timestamp.
+alias ts='date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s"'
+
 # Shortcut to compress and remove a file or directory.
 function czf() {
   FILE=$1
   tar -czf $FILE.tar.gz $FILE && rm -rf $FILE
+}
+
+# Extract above, and preserve original.
+function xzf() {
+  FILE=$1
+  tar -xzf $FILE
 }
