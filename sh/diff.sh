28b60c2eb634b54b72b11ddcaeabaf68f1c341d9
Carl Wiedemann <carl.wiedemann@gmail.com>
2013-11-20 17:01:11 -0700

    Just use wget

--- sh/diff.sh
+++ sh/diff.sh
@@ -1,9 +1,11 @@
 function ddiff() {
   URL=$1
   # Get clean output
-  http d8clean.localhost/$URL Cookie:$DRUPALCOOKIECLEAN > /tmp/d8clean-tmp.html
+  # http d8clean.localhost/$URL Cookie:$DRUPALCOOKIECLEAN > /tmp/d8clean-tmp.html
+  wget --no-cookies --header "Cookie: $DRUPALCOOKIECLEAN" http://d8clean.localhost/$URL -O /tmp/d8clean-tmp.html
   # Get patched output
-  http d8.localhost/$URL Cookie:$DRUPALCOOKIE > /tmp/d8.html
+  # http d8.localhost/$URL Cookie:$DRUPALCOOKIE > /tmp/d8.html
+  wget --no-cookies --header "Cookie: $DRUPALCOOKIE" http://d8.localhost/$URL -O /tmp/d8.html
   # Make domains similar
   sed -e 's/d8clean/d8/g' /tmp/d8clean-tmp.html > /tmp/d8clean.html
   # Kaleidescope diff
